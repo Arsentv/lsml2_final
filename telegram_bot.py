6b648e354a7574547703f04614661c188c26c25f
Arsentv <105422158+Arsentv@users.noreply.github.com>
2022-11-20 03:04:19 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,7 @@ import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
 
-telegram_bot_token = '5956945497:AAEkxlAuY9EyE0phy1PrQz4lnDFae77ZJg0'
+telegram_bot_token = ''
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
