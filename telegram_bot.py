a0d98afccede53a41f12f763e5007d8d8492ced9
Arsentv <105422158+Arsentv@users.noreply.github.com>
2022-11-20 13:32:59 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,9 +2,9 @@ import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
 
-telegram_bot_token = ''
+TELEGRAM_TOKEN = ''
 
-updater = Updater(token=telegram_bot_token, use_context=True)
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 # set up the introductory statement for the bot when the /start command is invoked
